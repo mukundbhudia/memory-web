54321d7e479469f7f4bfa55051dfe6ff60df0e17
Mukund Bhudia <mukund_bhudia@hotmail.com>
2020-05-19 12:19:50 +0100

    Added basic API endpoint connection

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,6 +4,16 @@
   <main role="main" class="flex-shrink-0">
     <div class="container">
       <Header/>
+      <section v-if="errored">
+        <p>We're sorry, we're not able to retrieve this information at the moment, please try back later</p>
+      </section>
+
+      <section v-else>
+        <div v-if="loading">Loading...</div>
+        <div v-else>
+          API says: {{ info }}
+        </div>
+      </section>
       <Map/>
       <LogEntryForm/>
     </div>
@@ -16,6 +26,7 @@
 import Header from '@/components/Header.vue'
 import Map from '@/components/Map.vue'
 import LogEntryForm from '@/components/LogEntryForm.vue'
+import axios from 'axios'
 
 export default {
   name: 'Home',
@@ -23,6 +34,26 @@ export default {
     Header,
     Map,
     LogEntryForm
+  },
+  data () {
+    return {
+      info: null,
+      loading: true,
+      errored: false
+    }
+  },
+  mounted () {
+    const URI = process.env.URI || 'http://localhost:4000'
+    axios
+      .get(`${URI}/`)
+      .then(response => {
+        this.info = response.data.msg
+      })
+      .catch(error => {
+        console.log(error)
+        this.errored = true
+      })
+      .finally(() => { this.loading = false })
   }
 }
 </script>
