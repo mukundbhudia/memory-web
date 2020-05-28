dac5a4d4339d44e5df49c8485c8643d179d400e3
Mukund Bhudia <mukund_bhudia@hotmail.com>
2020-05-28 18:14:09 +0100

    Updated navbar to be mobile responsive with menus

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,17 +1,32 @@
 <template>
-    <nav class="site-header sticky-top py-1">
-      <div class="container d-flex flex-column flex-md-row justify-content-between">
-        <router-link class="py-2" to="/" aria-label="Product">
-          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="d-block mx-auto" role="img" viewBox="0 0 24 24" focusable="false"><title>Product</title><circle cx="12" cy="12" r="10"/><path d="M14.31 8l5.74 9.94M9.69 8h11.48M7.38 12l5.74-9.94M9.69 16L3.95 6.06M14.31 16H2.83m13.79-4l-5.74 9.94"/></svg>
-        </router-link>
-        <router-link class="py-2 d-none d-md-inline-block" to="/">Home</router-link>
-        <router-link class="py-2 d-none d-md-inline-block" to="/notes">Notes</router-link>
-        <router-link class="py-2 d-none d-md-inline-block" to="/about">About</router-link>
-        <router-link v-if="loggedIn" class="py-2 d-none d-md-inline-block" to="/me">{{ getUser.fullName }}</router-link>
-        <router-link v-if="loggedIn" class="py-2 d-none d-md-inline-block" to="/logout">Logout</router-link>
-        <router-link v-else class="py-2 d-none d-md-inline-block" to="/login-signup">Login/Sign up</router-link>
-      </div>
-    </nav>
+  <div>
+    <b-navbar toggleable="lg" type="dark" variant="dark">
+      <b-navbar-brand href="#/">Memory</b-navbar-brand>
+
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav>
+          <b-nav-item href="#/">Home</b-nav-item>
+          <b-nav-item href="#/notes">Notes</b-nav-item>
+          <b-nav-item href="#/about">About</b-nav-item>
+        </b-navbar-nav>
+
+        <!-- Right aligned nav items -->
+        <b-navbar-nav class="ml-auto">
+          <b-nav-item-dropdown right v-if="loggedIn">
+            <!-- Using 'button-content' slot -->
+            <template v-slot:button-content>
+              <em>{{ getUser.fullName }}</em>
+            </template>
+            <b-dropdown-item href="#/me">My profile</b-dropdown-item>
+            <b-dropdown-item href="#/logout">Sign Out</b-dropdown-item>
+          </b-nav-item-dropdown>
+          <b-nav-item v-else href="#/login-signup">Login/Sign up</b-nav-item>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+  </div>
 </template>
 
 <script>
