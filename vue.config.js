0d99ccce244a4aa2a01fb23cfef471919612fb3e
Mukund Bhudia <mukund_bhudia@hotmail.com>
2020-05-28 12:03:37 +0100

    Added favicon, app description and changed page title

--- vue.config.js
+++ vue.config.js
@@ -3,5 +3,18 @@
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
     ? '/' + process.env.CI_PROJECT_NAME + '/'
-    : '/'
+    : '/',
+  pages: {
+    index: {
+      // entry for the page
+      entry: 'src/main.js',
+      // the source template
+      template: 'public/index.html',
+      // output as dist/index.html
+      filename: 'index.html',
+      // when using title option,
+      // template title tag needs to be <title><%= htmlWebpackPlugin.options.title %></title>
+      title: 'Memory'
+    }
+  }
 }
